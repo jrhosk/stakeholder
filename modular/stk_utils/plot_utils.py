c4f5f8a1a90b4cfc612fdfa0e8355f9e2978f5e7
Joshua Hoskins <jrh4dy@gmail.com>
2021-11-08 11:39:14 -0500

    Created new stk_utils module to handle stakeholder utility needs.

---
+++ modular/stk_utils/plot_utils.py
@@ -0,0 +1,28 @@
+def make_moment_plot(imname='',chan=0):
+    casatasks.immoments(imagename = imname, moments = 8, outfile = imname+'.moment8')
+
+    ia = casatools.image()
+
+    ia.open(imname + '.moment8')
+    pix = ia.getchunk()[:,:,0,chan]
+    csys = ia.coordsys()
+    ia.close()
+    shp = pix.shape
+
+    rad_to_deg =  180/np.pi
+    w = WCS(naxis=2)
+    w.wcs.crpix = csys.referencepixel()['numeric'][0:2]
+    w.wcs.cdelt = csys.increment()['numeric'][0:2]*rad_to_deg
+    w.wcs.crval = csys.referencevalue()['numeric'][0:2]*rad_to_deg
+    w.wcs.ctype = ['RA---SIN', 'DEC--SIN']
+
+    pl.subplot(projection=w)
+
+    p1 = int(shp[0]*0.25)
+    p2 = int(shp[0]*0.75)
+
+    pl.imshow(pix[p1:p2,p1:p2].transpose(), origin='lower',  cmap=pl.cm.viridis)
+    pl.xlabel('Right Ascension')
+    pl.ylabel('Declination')
+    
+    pl.imsave(imname + '.momen.png', pix[p1:p2,p1:p2].transpose())
